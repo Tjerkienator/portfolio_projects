0323656387665efd46495801bc3af09767de4742
Tjerkienator <douwe@fryslanecom.com>
2023-02-11 15:58:38 +0100

    updates

--- elon_musk_tweets/1_data_scraping_python/app/data/openAi_api_key.py
+++ elon_musk_tweets/1_data_scraping_python/app/data/openAi_api_key.py
@@ -1,5 +1,7 @@
+import os
+
 class openAiKey():
 
     # leaving openAiKey value empty for protection of my account
-    openAiKey = ""
+    openAiKey = os.getenv("openAiKey")
 
--- elon_musk_tweets/1_data_scraping_python/app/data/twitter_api_keys.py
+++ elon_musk_tweets/1_data_scraping_python/app/data/twitter_api_keys.py
@@ -1,12 +1,14 @@
+import os
+
 class TwitterApiKeys():
 
     # leaving twitterKeys value empty for protection of my account
     twitterKeys = {
-        "api_key": "",
-        "api_key_secret": "",
-        "access_token": "",
-        "access_token_secret": "",
-        "client_id": "",
-        "client_secret": "",
+        "api_key": os.getenv("TWITTER_API_KEY"),
+        "api_key_secret": os.getenv("TWITTER_API_SECRET"),
+        "access_token": os.getenv("TWITTER_ACCESS_TOKEN"),
+        "access_token_secret": os.getenv("TWITTER_ACCESS_SECRET"),
+        "client_id": os.getenv("TWITTER_CLIENT_ID"),
+        "client_secret": os.getenv("TWITTER_CLIENT_SECRET"),
     }
 
