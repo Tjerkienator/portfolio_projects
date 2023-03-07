8ec692560efd08a64a10d6fc41eb9f49dbd60b1f
Tjerkienator <douwe@fryslanecom.com>
2023-03-07 14:10:08 +0200

    added -- URL --

--- elon_musk_tweets/3_data_exploration_sql/exploration_queries.sql
+++ elon_musk_tweets/3_data_exploration_sql/exploration_queries.sql
@@ -4,7 +4,8 @@ Data Exploration using SQL Queries
 
 */
 
-
+SELECT *
+FROM tweet_data
 ------------------------------------------------------------------------------------------
 
 -- LANGUAGE --
@@ -29,3 +30,20 @@ ROUND(AVG(retweetCount),0) AS avg_retweets
 FROM tweet_data
 GROUP BY tweetLanFull
 ORDER BY 2 DESC, 3 DESC
+
+------------------------------------------------------------------------------------------
+
+-- URL --
+
+-- Average of engagement metrics per URL value
+-- Shows if adding a URL is of any effect to a tweets engagement
+
+SELECT DISTINCT(URL), 
+ROUND((COUNT(URL)/(SELECT COUNT(*) FROM tweet_data)*100), 2) AS percentage_of_tweets,
+ROUND(AVG(likeCount),0) AS avg_likes,
+ROUND(AVG(replyCount),0) AS avg_replies,
+ROUND(AVG(retweetCount),0) AS avg_retweets
+FROM tweet_data
+GROUP BY URL
+ORDER BY 2 DESC, 3 DESC
+
