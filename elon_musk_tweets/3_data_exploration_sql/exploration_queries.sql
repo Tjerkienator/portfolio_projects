7ca8436d58efd52640d856df150eaa555bca0468
Tjerkienator <douwe@fryslanecom.com>
2023-03-07 14:07:04 +0200

    added -- Average of engagement metrics per language

--- elon_musk_tweets/3_data_exploration_sql/exploration_queries.sql
+++ elon_musk_tweets/3_data_exploration_sql/exploration_queries.sql
@@ -7,24 +7,25 @@ Data Exploration using SQL Queries
 
 ------------------------------------------------------------------------------------------
 
--- Selecting data we are going to start with
-
-SELECT 
+-- LANGUAGE --
 
 -- Language used / total tweets
 -- Shows to what percentage the language was used on the total amount of tweets
 
-
-SELECT DISTINCT(tweetLanFull) AS tweet_language, ROUND((COUNT(tweetLanFUll)/(SELECT COUNT(*) FROM tweet_data)*100), 2) AS percentage_of_tweets
+SELECT DISTINCT(tweetLanFull) AS tweet_language, 
+ROUND((COUNT(tweetLanFUll)/(SELECT COUNT(*) FROM tweet_data)*100), 2) AS percentage_of_tweets
 FROM tweet_data
 GROUP BY tweetLanFull
 ORDER BY 2 DESC
-
 
 -- Average of engagement metrics per language
 -- Shows what language is most popular among Musks followers
 
-SELECT DISTINCT(tweetLanFull), COUNT(tweetLanFUll) AS times_used, AVG(likeCount), AVG(replyCount), AVG(retweetCount)
+SELECT DISTINCT(tweetLanFull), 
+ROUND((COUNT(tweetLanFUll)/(SELECT COUNT(*) FROM tweet_data)*100), 2) AS percentage_of_tweets,
+ROUND(AVG(likeCount),0) AS avg_likes,
+ROUND(AVG(replyCount),0) AS avg_replies,
+ROUND(AVG(retweetCount),0) AS avg_retweets
 FROM tweet_data
 GROUP BY tweetLanFull
-ORDER BY 2 DESC
+ORDER BY 2 DESC, 3 DESC
