8cc22f8724ede02658c0792a4478b69f4d87b346
Tjerkienator <douwe@fryslanecom.com>
2023-03-07 14:00:20 +0200

    added -- Language used / total tweets

--- elon_musk_tweets/3_data_exploration_sql/exploration_queries.sql
+++ elon_musk_tweets/3_data_exploration_sql/exploration_queries.sql
@@ -7,3 +7,24 @@ Data Exploration using SQL Queries
 
 ------------------------------------------------------------------------------------------
 
+-- Selecting data we are going to start with
+
+SELECT 
+
+-- Language used / total tweets
+-- Shows to what percentage the language was used on the total amount of tweets
+
+
+SELECT DISTINCT(tweetLanFull) AS tweet_language, ROUND((COUNT(tweetLanFUll)/(SELECT COUNT(*) FROM tweet_data)*100), 2) AS percentage_of_tweets
+FROM tweet_data
+GROUP BY tweetLanFull
+ORDER BY 2 DESC
+
+
+-- Average of engagement metrics per language
+-- Shows what language is most popular among Musks followers
+
+SELECT DISTINCT(tweetLanFull), COUNT(tweetLanFUll) AS times_used, AVG(likeCount), AVG(replyCount), AVG(retweetCount)
+FROM tweet_data
+GROUP BY tweetLanFull
+ORDER BY 2 DESC
